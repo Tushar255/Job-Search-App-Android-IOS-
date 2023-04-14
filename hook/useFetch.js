565b6e49e6f7af7ec1fc2fb103ba9c6e69dd87e9
Tushar <tusharbharane2001@gmail.com>
2023-04-14 11:40:43 +0530

    Custom Hook for fetching data

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -1,8 +1,5 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import dotenv from "dotenv"
-
-dotenv.config();
 
 const useFetch = (endpoint, query) => {
     const [data, setData] = useState([])
@@ -13,7 +10,7 @@ const useFetch = (endpoint, query) => {
         method: 'GET',
         url: `https://jsearch.p.rapidapi.com/${endpoint}`,
         headers: {
-            'X-RapidAPI-Key': process.env.RAPID_API_KEY,
+            'X-RapidAPI-Key': '',
             'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
         },
         params: { ...query },
